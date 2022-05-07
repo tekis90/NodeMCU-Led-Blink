5c4d0fbf36433aa77d5cd2c88ebafc9c79786f76
Mestan <83555018+tekis90@users.noreply.github.com>
2022-05-07 11:56:21 +0300

    Led Blink

---
+++ Blink.ino
@@ -0,0 +1,14 @@
+
+
+void setup() {
+  pinMode(5, OUTPUT);     
+}
+
+void loop() {
+  digitalWrite(5, LOW);  
+                                    
+                                    
+  delay(50);                     
+  digitalWrite(5, HIGH);  
+  delay(50);                      
+}
